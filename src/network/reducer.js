1f261ce8c06740e7720a85cdecaeb9e5710d17ee
kostimarko <kostimarko@gmail.com>
2018-10-04 17:31:27 -0500

    removed unneed state values from reducer. removed unneeded firebase and actions

--- src/network/reducer.js
+++ src/network/reducer.js
@@ -1,20 +1,16 @@
-import { APP_LOADED, GOT_WEEK_EVENTS, GOT_ALL_EVENTS } from './actionTypes';
+import { APP_LOADED, GOT_WEEK_EVENTS } from './actionTypes';
 
 const initEventState = {
   loaded: false,
-  Events: {},
-  AllEvents: {}
+  Events: {}
 };
 
 export const eventsReducer = (state = initEventState, action) => {
   switch (action.type) {
     case APP_LOADED:
-      console.log(action.data);
       return { ...state, loaded: action.data };
     case GOT_WEEK_EVENTS:
       return { ...state, Events: action.data };
-    case GOT_ALL_EVENTS:
-      return { ...state, AllEvents: action.data };
     default:
       return { ...state };
   }
