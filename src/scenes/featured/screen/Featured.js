1ddc293f7885ebb1c836cf69fb2b6375198e3ab8
kostimarko <kostimarko@gmail.com>
2019-01-12 17:30:23 -0600

    fixed more merge conflicts

--- src/scenes/featured/screen/Featured.js
+++ src/scenes/featured/screen/Featured.js
@@ -114,10 +114,6 @@ class Featured extends PureComponent {
     </View>
   );
   render() {
-<<<<<<< HEAD
-=======
-    console.log(this.props.Featured);
->>>>>>> feature/featured-events
     const { FlatListContainer } = styles;
     const color = {
       mainColor: '#E43F6F',
@@ -133,11 +129,7 @@ class Featured extends PureComponent {
           <StatusBar backgroundColor="white" barStyle="dark-content" hidden={false} />
           <View style={[FlatListContainer]}>
             <FlatList
-<<<<<<< HEAD
               data={this.props.Featured}
-=======
-              data={this.props.Events}
->>>>>>> feature/featured-events
               renderItem={this._renderItem}
               showsVerticalScrollIndicator={false}
               keyExtractor={(item, index) => item.id.toString()}
